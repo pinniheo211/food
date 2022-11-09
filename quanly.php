53f6c4df6849805d813b084cccfa83345ceca9fc
pinniheo211 <84260434+pinniheo211@users.noreply.github.com>
2022-11-09 23:49:13 +0700

    update quanlypage

--- quanly.php
+++ quanly.php
@@ -3,8 +3,10 @@
 require_once 'config/conn.php'?>
 
 
-
-
+<?php
+    $sql = "select *from monan";
+    $query = mysqli_query($conn,$sql);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -46,6 +48,47 @@ require_once 'config/conn.php'?>
         }
 
     ?>
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header">
+                <h2>Danh sach san pham</h2>
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>ten mon an</th>
+                            <th>chi tiet mon an</th>
+                            <th>so tien</th>
+                            <th>hinh anh</th>
+                            <th>sua</th>
+                            <th>xoa</th>
+
+                        </tr>
+                        <tbody>
+                            <?php
+                            $i =1;
+                                while($row = mysqli_fetch_assoc($query)){?>
+                                    <tr>
+                                        <td><?php echo $row['id']; ?></td>
+                                        <td><?php echo $row['tenmonan']; ?></td>
+                                        <td><?php echo $row ['mota'];  ?></td>
+                                        <td><?php echo $row ['tien']; ?></td>
+                                        <td><?php echo $row ['hinhanh']; ?></td>
+
+                                        <td>sua</td>
+                                        <td>xoa</td>
+                                    </tr>
+                            <?php } ?>
+
+                            
+                        </tbody>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
